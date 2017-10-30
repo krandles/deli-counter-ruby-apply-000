8125e22cd88c6a5d118bbd7d9ef5b125b2a6a974
Kevin Randles <krandles@gmail.com>
2017-10-30 12:25:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,10 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  puts "Currently serving #{array[0]}"
-  array.shift
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    puts "Currently serving #{array[0]}"
+    array.shift
+  end
 end
