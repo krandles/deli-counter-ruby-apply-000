831959e10e06d804238237211070aa95015da5b3
Kevin Randles <krandles@gmail.com>
2017-10-30 12:19:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+    names  = ""
+    array.each {|x| names += "#{x +1}. #{array[x]} "}
+    puts "The line is currently: " + names
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving()
+
+end
