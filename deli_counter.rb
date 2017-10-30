16c22c662901dc4aec64f3e2379e9d84f2e272eb
Kevin Randles <krandles@gmail.com>
2017-10-30 12:24:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,21 @@
 katz_deli = []
 
 def line(array)
-    names  = ""
-    array.each {|x| names += "#{x +1}. #{array[x]} "}
+  names  = ""
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.each {|x| names += "#{x + 1}. #{array[x]} "}
     puts "The line is currently: " + names
+  end
 end
 
 def take_a_number(array, name)
-
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving()
-
+def now_serving(array)
+  puts "Currently serving #{array[0]}"
+  array.shift
 end
